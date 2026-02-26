c9b5eca7e5314005c019d15675b2df10cc90051a
ITO Sukenori <114732830+sukenori@users.noreply.github.com>
2026-02-26 12:18:47 +0000

    26-02-26

---
+++ 26-02-26/AWC0013D.nim
@@ -0,0 +1,13 @@
+include "/workspaces/AtCoder-Nim/.Library/.Template/Template.nim"
+let N,M=int.input
+var A=Seq[M,N:int]
+for i in 0..<N:
+  for j in 0..<M:
+    A[j][i]=int.input
+for i in 0..<M:
+  A[i].sort
+var a=0
+for i in 0..<M:
+  for j in 1..<N:
+    a+=(A[i][j]-A[i][j-1])*((N-j)*j)
+echo a
